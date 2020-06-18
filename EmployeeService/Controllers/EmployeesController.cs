9da823da95538e2c0f5ae6f3af96222acb38f58f
NamathaS <namathascavish@hotmail.com>
2020-06-18 12:09:59 +1200

    Add project files.

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        EmployeeDBEntities db = new EmployeeDBEntities();
+
+        public IEnumerable<Employees> GetEmployees()
+        {
+            var EmployeesList = db.Employees.ToList();
+            return EmployeesList;
+        }
+    }
+}
